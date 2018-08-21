a5867d17a63e7aa4c97fc6db380dea66d1279a96
Daniel Kim <dkim@vlocity.com>
2018-08-21 14:13:20 -0700

    Added additional test lightning web components

---
+++ force-app/main/default/lightningcomponents/hello_world/hello_world.js
@@ -0,0 +1,7 @@
+import { Element, api } from 'engine';
+
+class hello_world extends Element {
+    @api greeting;
+}
+
+export default hello_world;
---
+++ force-app/main/default/lightningcomponents/playground/playground.js
@@ -0,0 +1,3 @@
+import { Element } from 'engine';
+
+export default class playground extends Element {}
